dfea7553642692f5b9bc5aa732335c11fc727859
Robin Ekman <robin.seth.ekman@gmail.com>
2022-05-01 22:36:10 +0200

    cover art: add option to copy from local file instead

--- remote.lua
+++ remote.lua
@@ -273,12 +273,18 @@ local function ui_update_repeats(message)
 end
 
 local function ui_update_cover_art(message)
-    if not message["cover-art-blob"] then
-        log.warn("cover art: no blob provided")
-        return
+    if not message["cover-art-blob"] and not message["cover-art-filename"] then
+      return
     end
     tmpf = fs.temp()
-    fs.write(tmpf, data.frombase64(message["cover-art-blob"]))
+    if message["cover-art-blob"] then
+      log.warn("writing cover from blob")
+      fs.write(tmpf, data.frombase64(message["cover-art-blob"]))
+    end
+    if message["cover-art-filename"] then
+      log.warn("copying cover from file")
+      fs.copy(message["cover-art-filename"], tmpf)
+    end
     server.update( {id = "cover_art", image=tmpf} )
 end
 
